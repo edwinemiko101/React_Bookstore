dbfae4c85a79b1a18279c5356c4d37846b758f01
edwinemiko101 <54147225+edwinemiko101@users.noreply.github.com>
2023-07-27 20:26:36 -0400

    second commit

--- src/components/AddBookForm.tsx
+++ src/components/AddBookForm.tsx
@@ -42,7 +42,27 @@ const AddBookForm: React.FC<Props> = ({ onClose, book }) => {
 
   return (
     <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-opacity-75 bg-black">
-      <div className="bg-white p-4 rounded-md shadow-lg">
+      <div className="bg-white p-4 rounded-md shadow-lg relative">
+        <button
+          className="absolute top-0 right-0 p-2 text-gray-600 hover:text-gray-800"
+          onClick={onClose}
+        >
+          {/* Close button (X icon) */}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
         <input
           type="text"
           placeholder="Name"
